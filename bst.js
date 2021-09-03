86f6d22ed3147268d5ab7253fb7cdb506e8839bb
siid sharma <siid123ck@gmail.com>
2021-09-03 22:09:35 +1000

    depth search tree pre order

--- bst.js
+++ bst.js
@@ -64,6 +64,47 @@ class BST{
         // return not found value or undefined (dont need to write return if need to return undefined)
         return `Not found value ${val}`
     }
+
+    // breadth search tree 
+    breadthST(){
+        // define data and queue to be empty array and currentRoot to be root
+        let data = []; 
+        let queue = []; 
+        let currentRoot = this.root; 
+        // push currentRoot in queue
+        queue.push(currentRoot); 
+
+        while(queue.length){
+            // set currentRoot to be shifted value from queue and push it in data
+            currentRoot = queue.shift(); 
+            data.push(currentRoot.val); 
+            
+            //push currentRoot.left in queue if it has left and current.right if it has right node
+            if(currentRoot.left) queue.push(currentRoot.left); 
+            if(currentRoot.right) queue.push(currentRoot.right); 
+        }
+        //return data at the end
+        return data; 
+    }
+
+    // depth search tree in preorder
+    dstPre(){
+        // define data to be empty array
+        let data = []; 
+
+        // create helper function to do recursion
+       function traverse(startNode){
+           // check if startNode has left, call helper function itself with argument left node
+           if(startNode.left) traverse(startNode.left); 
+           // check if startNode has right, call helper function itself with argument right node
+            if(startNode.right) traverse(startNode.right);
+            // finally push the node value in data
+            data.push(startNode.val);
+        }
+        //call helper function with argument root and return data
+        traverse(this.root);
+        return data;
+    }
 }
 
 // create instance of BST
